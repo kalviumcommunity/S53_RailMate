0a9697f28e64a97d7e04ab4dd6d742f8523d5f36
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-02-12 15:14:04 +0530

    All crud Routes created

--- Backend/route.js
+++ Backend/route.js
@@ -4,11 +4,32 @@ const router = express.Router();
 const dataModel = require("./scheema");
 app.use(express.json());
 
-router.get("/", async (req, res) => {
+router.get("/Train", async (req, res) => {
   try {
     const NewTrain = await dataModel.find();
     console.log("NewTrain: ", NewTrain);
     res.send(NewTrain);
+  } catch (err) {
+    console.log("error");
+  }
+});
+router.post("/CreateForm", async (req, res) => {
+  try {
+    res.send({message:true,data:"data added succesfully."})
+  } catch (err) {
+    console.log("error");
+  }
+});
+router.put("/UpdateTrainList", async (req, res) => {
+  try {
+    res.send({message:true,data:"data Updated succesfully."})
+  } catch (err) {
+    console.log("error");
+  }
+});
+router.delete("/DeleteForm", async (req, res) => {
+  try {
+    res.send({message:true,data:"data Deleted succesfully."})
   } catch (err) {
     console.log("error");
   }
