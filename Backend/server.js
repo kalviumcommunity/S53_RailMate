1b67d20df6368f3f091d8fbf641d68f1fe283e10
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-02-12 10:07:54 +0530

    Crud Routes made

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,11 @@ require("dotenv").config()
 const connectDB=require("./db")
 connectDB()
 const PORT = process.env.PORT || 3064
-const router=require("./routes")
+
 app.get('/', (req, res) => {
     res.send('Railway Mate Api is Working')
 })
-app.use("/Train",router)
+
 app.get("/ping", ((req, res) => {
     res.send("Vishnu Preethasm")
 }))
